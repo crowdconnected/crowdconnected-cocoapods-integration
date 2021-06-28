adbeaf210d4af5a51f0447bcb6ecf0f0f6290fd9
Robert Tanase <robert.tanase97@gmail.com>
2021-06-28 21:22:05 +0100

    Add TestCocoaPodsIntegration (SDK v1.0.0)

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+target 'TestCocoaPodsIntegration' do
+  use_frameworks!
+
+  pod 'CrowdConnectedIPS'
+  pod 'CrowdConnectedCore'
+
+end
--- TestCocoaPodsIntegration/TestCocoaPodsIntegrationApp.swift
+++ TestCocoaPodsIntegration/TestCocoaPodsIntegrationApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import CrowdConnectedIPS
+import CrowdConnectedCore
+import CoreLocation
 
 @main
 struct TestCocoaPodsIntegrationApp: App {
+    
+    let locationsProvider = LocationsProvider()
+    let locationManager = CLLocationManager()
+
+    init() {
+        CrowdConnectedIPS.activate()
+        CrowdConnected.shared.start(appKey: "testKey", token: "iosuser", secret: "Ea80e182$") { deviceId, error in
+            if let deviceId = deviceId {
+                print("Device id \(deviceId)")
+            }
+            if let error = error {
+                print("Error \(error)")
+            }
+        }
+        
+        CrowdConnected.shared.delegate = locationsProvider
+        
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class LocationsProvider: CrowdConnectedDelegate {
+    func didUpdateLocation(_ locations: [Location]) {
+        print("Got locations \(locations)")
     }
 }
