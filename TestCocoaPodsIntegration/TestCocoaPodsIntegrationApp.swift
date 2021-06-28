2e33463086e1f7fdc936037b59988dc5e3b63211
Robert Tanase <robert.tanase97@gmail.com>
2021-06-28 19:12:02 +0100

    Initial Commit

---
+++ TestCocoaPodsIntegration/TestCocoaPodsIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCocoaPodsIntegrationApp.swift
+//  TestCocoaPodsIntegration
+//
+//  Created by Robert Tanase on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestCocoaPodsIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
